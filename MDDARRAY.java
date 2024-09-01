c4a88288b85cfab3f043f91c8a3c164f34ef9706
Wasif Khan <m.wasif9905@gmail.com>
2024-09-01 16:11:55 +0500

    Add files via upload

---
+++ MDDARRAY.java
@@ -0,0 +1,41 @@
+public class MDDARRAY {
+    public static void main(String[]args){
+        /*int [][][] arr={{{1,2,3,4,5},{1,2,3},{1}}};
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[i].length;j++){
+                for(int k=0;k<arr[i][j].length;k++){
+                    System.out.print(arr[i][j][k]+" ");
+                }
+                System.out.println();
+            }
+        }
+    }
+  int[][][] a;
+  a=new int[2][3][4];
+  a[0][1][1]=20;
+System.out.print(a[0][1][1]);
+int[][][] array={{{10,20},{30,40,50,60},{70,80,90}}};
+System.out.println(array[0][0][0]);
+System.out.println(array[0][0][1]);
+System.out.println(array[0][1][0]);
+System.out.println(array[0][1][1]);
+System.out.println(array[0][1][2]);
+System.out.println(array[0][2][2]);
+System.out.println(array.length);
+System.out.println(array[0][0].length);
+
+int[][][] a;
+a=new int[2][4][5];
+a[0][0][1]=99;
+System.out.println(a[0][0][1]);*/
+int [][][] arr={{{1,1,1,1},{2}}};
+for(int i=0;i<arr.length;i++){
+    for(int j=0;j<arr[i].length;j++){
+        for(int k=0;k<arr[i][j].length;k++){
+            System.out.print(arr[i][j][k]+"                                ");
+        }
+        System.out.println();
+    }
+}
+}
+}
